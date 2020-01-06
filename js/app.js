be72d35b96b0d20269c99de1782a15a7373b1cd2
ing-arriola <ingeniero.arriola@gmail.com>
2020-01-06 04:04:32 -0600

    Added styles changes if fields of email are empty

--- js/app.js
+++ js/app.js
@@ -7,8 +7,33 @@ eventListeners()
 
 function eventListeners(){
     document.addEventListener('DOMContentLoaded',AppStart)
+    email.addEventListener('blur',validateField)
+    asunto.addEventListener('blur',validateField)
+    mensaje.addEventListener('blur',validateField)
 }
 
 function AppStart (){
     btnEnviar.disabled=true
+}
+//Function to validate Field
+function validateField(){
+    validateLong(this)
+
+    if(email.value !== '' && asunto.value !== ' ' && mensaje.value !== ' '){
+
+    }
+
+
+}
+
+//Arrow functions that add styles to the fields depending if it has a value or is empty
+const validateLong = (field) => {
+    console.log(field.value.length)
+    if (field.value.length>0){
+        field.style.borderBottomColor='green'
+        field.classList.remove('error')
+    }else{
+        field.style.borderBottomColor='red'
+        field.classList.add('error')
+    }
 }
