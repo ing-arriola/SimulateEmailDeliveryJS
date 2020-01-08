cffb00d5296d880d9fd09a3b94fe1aeec66941c1
ing-arriola <ingeniero.arriola@gmail.com>
2020-01-08 04:20:29 -0600

    Added a function to show some gif on the page

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ const email=document.getElementById('email')
 const asunto=document.getElementById('asunto')
 const mensaje= document.getElementById('mensaje')
 const btnEnviar=document.getElementById('enviar')
+const emailForm=document.getElementById('enviar-mail')
+const btnReset= document.getElementById('resetBtn')
 
 eventListeners()
 
@@ -10,6 +12,8 @@ function eventListeners(){
     email.addEventListener('blur',validateField)
     asunto.addEventListener('blur',validateField)
     mensaje.addEventListener('blur',validateField)
+    btnEnviar.addEventListener('click',sendEmail)
+    btnReset.addEventListener('click',resetForm)
 }
 
 function AppStart (){
@@ -33,6 +37,34 @@ function validateField(){
 
 
 }
+
+function sendEmail(e){
+    e.preventDefault()
+    //Select the spninner from the DOM, at this point it is currently hidden
+    const spinner=document.querySelector('#spinner')
+    //Show the spinner
+    spinner.style.display='block'
+    //Create a element in the DOM for the image of the letter gif
+    const sent=document.createElement('img')
+    //set the source for the image
+    sent.src='img/mail.gif'
+    sent.style.display='block'
+    //Set a asynchronous function that will set the display attribute of the spinner to none
+    //and will show the letter gif after 3 seconds after the spinner is showed
+    setTimeout(() => {
+       spinner.style.display='none'
+       document.querySelector('#loaders').appendChild(sent)
+       //Set a asynchronous function that will remove the letter gif from the DOM and it will reset
+       //the form 4 seconds after the letter gif is added to the DOM
+        setTimeout(() => {
+            sent.remove()
+            emailForm.reset()
+        }, 4000);
+    }, 3000);
+
+}
+
+
 
 //Arrow functions that add styles to the fields depending if it has a value or is empty
 const validateLong = (field) => {
